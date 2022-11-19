ebe9cccdadfbfe0b6c49a2ecce551f80570000de
Travis Collins <travistx@gmail.com>
2022-11-19 12:59:48 -0700

    clear output before running tests

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -1,7 +1,8 @@
 vim.g['test#neovim#start_normal'] = 1
 vim.g['test#preserve_screen'] = 0
--- run in quickfix (display with `:copen`)
 vim.g['test#strategy'] = 'kitty'
+
+vim.g['test#ruby#rspec#executable'] = 'clear; printf "\\e[3J"; rspec'
 
 vim.keymap.set('n', '<leader>t', ':wa<CR>:TestNearest<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>l', ':wa<CR>:TestLast<CR>', { noremap = true })
