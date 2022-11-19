4c7486180a6bc16910a0d9d34608d12882bef2e2
Travis Collins <travistx@gmail.com>
2022-11-19 12:58:35 -0700

    show hidden files in tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -164,9 +164,9 @@ require('neo-tree').setup({
   filesystem = {
     filtered_items = {
       visible = false, -- when true, they will just be displayed differently than normal items
-      hide_dotfiles = true,
-      hide_gitignored = true,
-      hide_hidden = true, -- only works on Windows for hidden files/directories
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_hidden = false, -- only works on Windows for hidden files/directories
       hide_by_name = {
         --"node_modules"
       },
