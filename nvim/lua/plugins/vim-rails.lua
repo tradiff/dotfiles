8f12a32a69976a2fe58da9862076acff1bf0948e
Travis Collins <travistx@gmail.com>
2022-12-19 17:05:02 -0700

    alternate for request specs thanks @djpowers

--- nvim/lua/plugins/vim-rails.lua
+++ nvim/lua/plugins/vim-rails.lua
@@ -1,3 +1,22 @@
 -- alternate file
 -- create the file if it's missing
 vim.keymap.set('n', '<leader>a', ':execute "e " . eval("rails#buffer().alternate()")<cr>', { noremap = true })
+
+vim.g.rails_projections = {
+  ['app/controllers/*_controller.rb'] = {
+    ['test'] = {
+      'spec/requests/{}_spec.rb',
+      'spec/controllers/{}_controller_spec.rb',
+      'test/controllers/{}_controller_test.rb'
+    },
+    ['alternate'] = {
+      'spec/requests/{}_spec.rb',
+      'spec/controllers/{}_controller_spec.rb',
+      'test/controllers/{}_controller_test.rb'
+    },
+  },
+  ['spec/requests/*_spec.rb'] = {
+    ['command'] = 'request',
+    ['alternate'] = 'app/controllers/{}_controller.rb',
+  },
+}
