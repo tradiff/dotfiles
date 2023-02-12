bf20ac5cf98cd2424ad25c5c5dfe1ec9f4e1dcb2
Travis Collins <travistx@gmail.com>
2023-02-12 05:42:07 -0700

    update rspec executable to use cls and ./bin/

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -2,7 +2,7 @@ vim.g['test#neovim#start_normal'] = 1
 vim.g['test#preserve_screen'] = 0
 vim.g['test#strategy'] = 'kitty'
 
-vim.g['test#ruby#rspec#executable'] = 'clear; printf "\\e[3J"; rspec'
+vim.g['test#ruby#rspec#executable'] = 'cls; ./bin/rspec'
 
 vim.keymap.set('n', '<leader>t', ':wa<CR>:TestNearest<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>l', ':wa<CR>:TestLast<CR>', { noremap = true })
