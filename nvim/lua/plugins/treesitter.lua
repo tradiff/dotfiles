438e00b5aac18417cb30543a726a8da703838c41
Travis Collins <travistx@gmail.com>
2023-02-12 05:40:52 -0700

    disable incremental_selection because it keeps occasionally showing an error when opening files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,10 +5,6 @@ require('nvim-treesitter.configs').setup({
   highlight = { enable = true },
   indent = { enable = true },
   incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<CR>',
-      scope_incremental = '<CR>',
-    },
+    enable = false,
   },
 })
