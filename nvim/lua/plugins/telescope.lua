a4efdcc3568f3a015deacde08873e8854dab7c7e
Travis Collins <travistx@gmail.com>
2023-03-04 08:26:36 -0700

    improve telescope settings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,14 +4,19 @@ local actions = require('telescope.actions')
 -- clear the selection highlight
 vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = 'bg' })
 
--- context-specific highlights
-vim.api.nvim_set_hl(0, 'TelescopeSpec', { bg = '#103235' })
-vim.api.nvim_set_hl(0, 'TelescopeAdmin', { bg = '#272D43' })
+-- used to feed the current selected text into telescope
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
 
-local highlights = {
-  { 'spec/.*', 'TelescopeSpec' },
-  { 'app/admin/.*', 'TelescopeAdmin' }
-}
+  text = string.gsub(text, '\n', '')
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
 
 telescope.setup({
   defaults = {
@@ -22,10 +27,21 @@ telescope.setup({
       local directory = string.sub(path, 1, -(string.len(tail) + 1))
       return string.format('%s (%s)', tail, directory)
     end,
+    border = true,
+    borderchars = {
+      prompt = { '─', '│', 'x', '│', '╭', '┬', '│', '│' },
+      results = { '─', '│', '─', '│', '├', '┤', '┴', '╰' },
+      preview = { '─', '│', '─', ' ', '─', '╮', '╯', '─' },
+    },
+    results_title = false,
     layout_strategy = 'horizontal',
+    sorting_strategy = 'ascending',
     layout_config = {
       horizontal = {
-        width = 0.99
+        width = 99999999,
+        height = 99999999,
+        prompt_position = 'top',
+        preview_width = 0.7,
       }
     },
     mappings = {
@@ -44,18 +60,8 @@ telescope.setup({
   pickers = {
     find_files = {
       hidden = false,
-      entry_maker = require 'plugins.telescope_make_entry'.gen_from_file(
-        {
-          highlights = highlights
-        }
-      ),
     },
     live_grep = {
-      entry_maker = require 'plugins.telescope_make_entry'.gen_from_vimgrep(
-        {
-          highlights = highlights
-        }
-      ),
     },
     lsp_references = {
       show_line = false
@@ -65,13 +71,25 @@ telescope.setup({
 
 telescope.load_extension('fzf')
 
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find,
-  { desc = '[/] Fuzzily search in current buffer]' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').git_status, { desc = 'Git Status' })
-vim.keymap.set('n', '<leader>fs', require('telescope.builtin').lsp_document_symbols, { desc = '[F]ind LSP [S]ymbols' })
+local keymap = vim.keymap.set
+local tb = require('telescope.builtin')
+local opts = { noremap = true, silent = true }
+
+keymap('n', '<leader>?', tb.oldfiles, { desc = '[?] Find recently opened files' }, opts)
+keymap('n', '<leader><space>', tb.buffers, { desc = '[ ] Find existing buffers' }, opts)
+keymap('n', '<leader>/', tb.current_buffer_fuzzy_find,
+  { desc = '[/] Fuzzily search in current buffer]' }, opts)
+keymap('n', '<leader>ff', tb.find_files, { desc = '[F]ind [F]iles' }, opts)
+keymap('n', '<leader>fh', tb.help_tags, { desc = '[F]ind [H]elp' }, opts)
+keymap('n', '<leader>fw', tb.grep_string, { desc = '[F]ind current [W]ord' }, opts)
+keymap('n', '<leader>fg', tb.live_grep, { desc = '[F]ind by [G]rep' }, opts)
+keymap('v', '<leader>fg', function()
+  local text = vim.getVisualSelection()
+  tb.live_grep({ default_text = text, desc = '[F]ind by [G]rep' })
+end, opts)
+
+keymap('n', '<leader>fd', tb.git_status, { desc = 'Git Status' }, opts)
+keymap('n', '<leader>fs', tb.lsp_document_symbols, { desc = '[F]ind LSP [S]ymbols' }, opts)
+
+vim.cmd [[autocmd User TelescopePreviewerLoaded setlocal wrap]]
+vim.cmd [[autocmd User TelescopePreviewerLoaded setlocal number]]
