8c7a862870009c0981b6fcbf7587499b91eda53f
Travis Collins <travistx@gmail.com>
2023-07-08 16:00:13 -0600

    fix nvim-lspconfig's breaking change to rubocop

--- nvim/lua/lsp/lspconfig.lua
+++ nvim/lua/lsp/lspconfig.lua
@@ -29,9 +29,11 @@ lspconfig.solargraph.setup({
 })
 
 lspconfig.rubocop.setup({
-  cmd = { "rubocop", "--lsp", },
   on_attach = on_attach,
   capabilities = capabilities,
+  on_new_config = function (new_config, new_root_dir)
+    new_config.cmd = { "rubocop", "--lsp", }
+  end,
 })
 
 lspconfig.lua_ls.setup({
