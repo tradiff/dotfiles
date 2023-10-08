f225717ed1277c9295c08e6492ba4c9b558ba199
Travis Collins <travistx@gmail.com>
2023-10-08 10:53:41 -0600

    update indent-blankline

--- nvim/lua/plugins/indent_blankline.lua
+++ nvim/lua/plugins/indent_blankline.lua
@@ -1,17 +1,9 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  config = function ()
-    require("indent_blankline").setup({
+  main = "ibl",
+  opts = {
+    indent = {
       char = "▏",
-      context_char = "▏",
-
-      show_current_context = true,
-
-      show_first_indent_level = true,
-      show_foldtext = false,
-      show_trailing_blankline_indent = false,
-      use_treesitter = true,
-      max_indent_increase = 1,
-    })
-  end,
+    },
+  },
 }
