970ca12effa0725756d819b330071fe46ec19734
Travis Collins <travistx@gmail.com>
2023-12-07 18:35:48 -0700

    format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local config = {}
 
@@ -6,23 +6,23 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Dracula'
+config.color_scheme = "Dracula"
 config.colors = {
-  cursor_bg = '#CD974C',
-  cursor_border = '#CD974C',
-  cursor_fg = 'black',
+  cursor_bg = "#CD974C",
+  cursor_border = "#CD974C",
+  cursor_fg = "black",
   tab_bar = {
-    background = '#0b0022',
+    background = "#0b0022",
     new_tab = {
-      bg_color = '#0b0022',
-      fg_color = '#808080',
-      intensity = 'Bold',
+      bg_color = "#0b0022",
+      fg_color = "#808080",
+      intensity = "Bold",
     },
   },
 }
 config.use_fancy_tab_bar = false
 
-config.font = wezterm.font('MesloLGS Nerd Font')
+config.font = wezterm.font("MesloLGS Nerd Font")
 config.font_size = 13.0
 config.warn_about_missing_glyphs = false
 
@@ -35,10 +35,11 @@ config.window_padding = {
 }
 config.mouse_wheel_scrolls_tabs = false
 
-config.default_cursor_style = 'SteadyBar'
-config.cursor_thickness = '2px'
+config.default_cursor_style = "SteadyBar"
+config.cursor_thickness = "2px"
 
 config.tab_max_width = 20
+config.hide_tab_bar_if_only_one_tab = true
 
 
 -- This function returns the suggested title for a tab.
@@ -56,7 +57,7 @@ function tab_title(tab_info)
   return tab_info.active_pane.title
 end
 
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover)
+wezterm.on("format-tab-title", function (tab, tabs, panes, config, hover)
   local min_width = 3
   local max_width = config.tab_max_width
 
@@ -67,24 +68,24 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover)
   -- right edge of tabs
   local SOLID_RIGHT_ARROW = utf8.char(0xe0bc)
 
-  local SUP_IDX = { '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹', '¹⁰', '¹¹',
-    '¹²', '¹³', '¹⁴', '¹⁵', '¹⁶', '¹⁷', '¹⁸', '¹⁹', '²⁰' }
+  local SUP_IDX = { "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹", "¹⁰", "¹¹",
+    "¹²", "¹³", "¹⁴", "¹⁵", "¹⁶", "¹⁷", "¹⁸", "¹⁹", "²⁰", }
 
-  local edge_background = '#0b0022'
-  local background = '#000000'
-  local foreground = '#ffffff'
-  local tab_num_fg = '#f8f8f2'
+  local edge_background = "#0b0022"
+  local background = "#000000"
+  local foreground = "#ffffff"
+  local tab_num_fg = "#f8f8f2"
 
   if tab.is_active then
-    background = '#bd93f9'
-    foreground = '#282a36'
-    tab_num_fg = '#282a36'
+    background = "#bd93f9"
+    foreground = "#282a36"
+    tab_num_fg = "#282a36"
   elseif hover then
-    background = '#6272a4'
-    foreground = '#f8f8f2'
+    background = "#6272a4"
+    foreground = "#f8f8f2"
   else
-    background = '#282a36'
-    foreground = '#b3b3af'
+    background = "#282a36"
+    foreground = "#b3b3af"
   end
 
   local edge_foreground = background
@@ -103,17 +104,17 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover)
   end
 
   return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = left_edge },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Foreground = { Color = tab_num_fg } },
-    { Text = id },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
+    { Background = { Color = edge_background, }, },
+    { Foreground = { Color = edge_foreground, }, },
+    { Text = left_edge, },
+    { Background = { Color = background, }, },
+    { Foreground = { Color = foreground, }, },
+    { Text = title, },
+    { Foreground = { Color = tab_num_fg, }, },
+    { Text = id, },
+    { Background = { Color = edge_background, }, },
+    { Foreground = { Color = edge_foreground, }, },
+    { Text = SOLID_RIGHT_ARROW, },
   }
 end)
 
@@ -126,70 +127,70 @@ config.mouse_bindings = {
   -- Change the default click behavior so that it only selects
   -- text and doesn't open hyperlinks
   {
-    mods = 'NONE',
-    event = { Up = { streak = 1, button = 'Left' } },
-    action = wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection',
+    mods = "NONE",
+    event = { Up = { streak = 1, button = "Left", }, },
+    action = wezterm.action.CompleteSelection "ClipboardAndPrimarySelection",
   },
   -- and make CTRL-Click open hyperlinks
   {
-    mods = 'CMD',
-    event = { Up = { streak = 1, button = 'Left' } },
+    mods = "CMD",
+    event = { Up = { streak = 1, button = "Left", }, },
     action = wezterm.action.OpenLinkAtMouseCursor,
   },
   -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
   {
-    mods = 'CMD',
-    event = { Down = { streak = 1, button = 'Left' } },
+    mods = "CMD",
+    event = { Down = { streak = 1, button = "Left", }, },
     action = wezterm.action.Nop,
   },
 }
 
 config.keys = {
   {
-    mods = 'CMD',
-    key = 'f',
+    mods = "CMD",
+    key = "f",
     action = wezterm.action.TogglePaneZoomState,
   },
   {
-    mods = 'CMD',
-    key = 's',
+    mods = "CMD",
+    key = "s",
     action = wezterm.action.SplitHorizontal,
   },
   {
-    mods = 'CMD|SHIFT',
-    key = 's',
+    mods = "CMD|SHIFT",
+    key = "s",
     action = wezterm.action.SplitVertical,
   },
   {
-    mods = 'CMD',
-    key = 'k',
-    action = wezterm.action.ClearScrollback('ScrollbackAndViewport'),
+    mods = "CMD",
+    key = "k",
+    action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
   },
   {
-    mods = 'CMD',
-    key = 't',
+    mods = "CMD",
+    key = "t",
     action = wezterm.action.SpawnCommandInNewTab({
-      cwd = wezterm.home_dir
+      cwd = wezterm.home_dir,
     }),
   },
   {
-    mods = 'CMD',
-    key = '[',
+    mods = "CMD",
+    key = "[",
     action = wezterm.action.MoveTabRelative(-1),
   },
   {
-    mods = 'CMD',
-    key = ']',
+    mods = "CMD",
+    key = "]",
     action = wezterm.action.MoveTabRelative(1),
   },
   {
-    mods = 'CMD',
-    key = 'UpArrow',
+    mods = "CMD",
+    key = "UpArrow",
     action = wezterm.action.ScrollByLine(-5),
   },
   {
-    mods = 'CMD',
-    key = 'DownArrow',
+    mods = "CMD",
+    key = "DownArrow",
     action = wezterm.action.ScrollByLine(5),
   },
 }
