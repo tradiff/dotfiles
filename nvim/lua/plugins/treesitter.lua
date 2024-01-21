a73ee64a8e7f5d37c0908245e303011d6cb08121
Travis Collins <travistx@gmail.com>
2024-01-21 12:16:41 -0700

    pin to older nvim-treesitter for now

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  -- pinned until vim-nightfly-colors is updated for the new treesitter captures
+  -- https://github.com/bluz71/vim-nightfly-colors/issues/57
+  version = "0.9.2",
   build = ":TSUpdate",
   config = function ()
     require("nvim-treesitter.configs").setup({
