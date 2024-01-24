c2162f595d612359b7bce820d8e8bfb5198e0003
Travis Collins <travistx@gmail.com>
2024-01-23 18:44:31 -0700

    display

--- bin/display-single.sh
+++ bin/display-single.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xrandr --dpi 96 \
-  --output eDP-1 --primary --mode 3456x2160 --pos 0x0 --rotate normal --scale 0.5x0.5 \
+  --output eDP-1 --primary --mode 3456x2160 --pos 0x0 --rate 60.00 --rotate normal --scale 0.5x0.5 \
   --output DP-1 --off \
   --output HDMI-1 --off \
   --output DP-2 --off \
