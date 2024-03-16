0b80d8885b8aa2635cac89d461511eee6cc38985
Travis Collins <travistx@gmail.com>
2024-03-16 07:12:43 -0600

    move laptop to the right

--- bin/display-ultrawide.sh
+++ bin/display-ultrawide.sh
@@ -9,9 +9,9 @@ xrandr --dpi 96 \
   --output DP-4 --off
 
 xrandr --dpi 96 \
-  --output eDP-1 --mode 3456x2160 --pos 0x0 --rate 60.00 --rotate normal --scale 0.5x0.5 \
+  --output eDP-1 --mode 3456x2160 --pos 5120x0 --rate 60.00 --rotate normal --scale 0.5x0.5 \
   --output DP-1 --off \
   --output HDMI-1 --off \
-  --output DP-2 --primary --mode 5120x1440 --pos 1728x0 --rate 100.00 --rotate normal --scale 0.9999x0.9999 \
+  --output DP-2 --primary --mode 5120x1440 --pos 0x0 --rate 100.00 --rotate normal --scale 0.9999x0.9999 \
   --output DP-3 --off \
   --output DP-4 --off
