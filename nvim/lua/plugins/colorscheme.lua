ceb0a85fef2cf3351e5468153264edaaca871013
Travis Collins <travistx@gmail.com>
2024-03-17 22:51:04 -0600

    neovim color scheme tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,60 +1,23 @@
 -- colorscheme
 
-local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "nightfly",
-  group = custom_highlight,
-  callback = function ()
-    local set_hl = vim.api.nvim_set_hl
-    local get_fg = function (name)
-      return vim.api.nvim_get_hl_by_name(name, true).foreground
-    end
-
-    set_hl(0, "DiffAdd", { bg = "#103235", })
-    set_hl(0, "DiffChange", { bg = "#272D43", })
-    set_hl(0, "DiffText", { bg = "#394B70", })
-    set_hl(0, "DiffviewDiffAddAsDelete", { bg = "#3F2D3D", })
-    set_hl(0, "DiffviewDiffDelete", { fg = "#3B4252", })
-    set_hl(0, "DiffDelete", { fg = "#21252A", bg = "#1A1D21", })
-
-    set_hl(0, "NeoTreeGitUntracked", { link = "NightflyGreyBlue", })
-
-    set_hl(0, "MatchParen", { link = "NightflyEmeraldMode", })
-    set_hl(0, "MatchWord", { link = "NightflyEmeraldMode", })
-    set_hl(0, "MatchParenCur", { link = "MatchWordCur", })
-
-    local gutter_bg = "#011322"
-
-    set_hl(0, "LineNr", { bg = gutter_bg, fg = get_fg("LineNr"), })
-    set_hl(0, "CursorLineNr", { bg = gutter_bg, fg = get_fg("NightflyOrange"), })
-
-    set_hl(0, "SignColumn", { bg = gutter_bg, })
-    set_hl(0, "GitSignsAdd", { bg = gutter_bg, fg = get_fg("NightflyEmerald"), })
-    set_hl(0, "GitSignsChange", { bg = gutter_bg, fg = get_fg("NightflyMalibu"), })
-    set_hl(0, "GitSignsDelete", { bg = gutter_bg, fg = get_fg("NightflyRed"), })
-
-    set_hl(0, "FoldColumn", { bg = gutter_bg, fg = get_fg("NightflyMalibu"), })
-    set_hl(0, "Folded", { link = "NightflyBlueLineActive", })
-
-    set_hl(0, "VertSplit", { link = "NightflyMalibu", })
-  end,
-})
 
 return {
-  "bluz71/vim-nightfly-colors",
+  "folke/tokyonight.nvim",
   name = "colorscheme",
   lazy = false,
   priority = 1000,
-  config = function ()
-    vim.g.nightflyCursorColor = true
-    vim.g.nightflyVirtualTextColor = false
+  config = function()
+    require("tokyonight").setup({
+      dim_inactive = true, -- dims inactive windows
 
-    -- split separators
-    vim.g.nightflyWinSeparator = 2
-    vim.diagnostic.config({
-      underline = false,
+      on_highlights = function(hl, _c)
+        hl.DiffAdd = { bg = "#103235", }
+        hl.DiffChange = { bg = "#272D43", }
+        hl.DiffText = { bg = "#394B70", }
+        hl.DiffviewDiffDelete = { fg = "#3B4252", }
+      end,
     })
 
-    vim.cmd([[colorscheme nightfly]])
+    vim.cmd([[colorscheme tokyonight-night]])
   end,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- pinned until vim-nightfly-colors is updated for the new treesitter captures
-  -- https://github.com/bluz71/vim-nightfly-colors/issues/57
-  version = "0.9.2",
   build = ":TSUpdate",
-  config = function ()
+  config = function()
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all"
       ensure_installed = { "help", "lua", "ruby", "vim", },
