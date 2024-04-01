4b80ced0e00e22129d4258ebd7f72abf3d890791
Travis Collins <travistx@gmail.com>
2024-03-31 23:39:11 -0600

    vue

--- nvim/lua/lsp/lspconfig.lua
+++ nvim/lua/lsp/lspconfig.lua
@@ -96,3 +96,21 @@ lspconfig.rust_analyzer.setup({
   },
 })
 
+local mason_registry = require("mason-registry")
+local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path() ..
+    "/node_modules/@vue/language-server"
+
+lspconfig.tsserver.setup({
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+})
+
+lspconfig.volar.setup({})
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,10 @@ return {
     enabled = true,
     config = function()
       require("conform").setup({
+        formatters_by_ft = {
+          vue = { "prettier" },
+        },
+
         format_on_save = {
           timeout_ms = 1000,
           lsp_fallback = true,
