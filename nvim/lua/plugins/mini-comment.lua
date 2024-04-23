0c93b14a4921deba8910d03640638f2a3ff182e3
Travis Collins <travistx@gmail.com>
2024-04-22 23:31:33 -0600

    kdl files and mini-comment

---
+++ nvim/lua/plugins/mini-comment.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      config = {
+        kdl = { __default = "// %s", __multiline = "/* %s */", },
+      },
+    },
+  },
+  {
+    "echasnovski/mini.comment",
+    config = function ()
+      require("mini.comment").setup({
+        options = {
+          custom_commentstring = function ()
+            return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+          end,
+        },
+      })
+    end,
+  },
+}
