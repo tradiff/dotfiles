b467c55784a011b3d80d692af9b3b31830844e3b
Travis Collins <travistx@gmail.com>
2024-06-30 23:36:23 -0600

    updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,14 @@ return {
   build = ":TSUpdate",
   config = function()
     vim.filetype.add({
+      extension = {
+        rasi = "rasi",
+      },
       pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
     })
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all"
-      ensure_installed = { "lua", "ruby", "vim", "typescript", "css", "scss", "kdl", "hyprlang", "markdown" },
+      ensure_installed = { "lua", "ruby", "vim", "typescript", "css", "scss", "kdl", "hyprlang", "markdown", "rasi" },
       highlight = { enable = true, },
       indent = { enable = true, },
       incremental_selection = {
