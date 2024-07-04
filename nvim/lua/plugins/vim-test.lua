f4eb4b40ce782da069c7ba98dbaad769971fa350
Travis Collins <travistx@gmail.com>
2024-07-04 16:19:50 -0600

    try vimux

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -1,13 +1,15 @@
 -- unit tests
 return {
   "vim-test/vim-test",
+  dependencies = { "preservim/vimux" },
   config = function()
     vim.g["test#neovim#start_normal"] = 1
     vim.g["test#preserve_screen"] = 0
-    vim.g["test#strategy"] = "shtuff"
+    vim.g["test#strategy"] = "vimux"
     vim.g["shtuff_receiver"] = "vim-test"
 
-    vim.g["test#ruby#rspec#executable"] = "cls; ./bin/rspec"
+    vim.g.VimuxOrientation = "h"
+    vim.g.VimuxHeight = "30"
 
     vim.keymap.set("n", "<leader>t", ":wa<CR>:TestNearest<CR>", { noremap = true, })
     vim.keymap.set("n", "<leader>l", ":wa<CR>:TestLast<CR>", { noremap = true, })
