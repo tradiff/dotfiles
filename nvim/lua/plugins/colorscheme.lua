0d4854577f10ed1805501396280529905db37599
Travis Collins <travistx@gmail.com>
2024-07-04 15:44:39 -0600

    nvim color adjustments

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,16 +10,37 @@ return {
       style = "night",
       dim_inactive = true, -- dims inactive windows
 
-      on_highlights = function(hl, _c)
-        hl.DiffAdd = { bg = "#103235", }
-        hl.DiffDelete = { bg = "#37222c", fg = "#1a1b26", }
-        hl.Comment = { fg = "#727ca7" } -- bit more contrast
-        hl.Folded = { bg = "#1a1b26" }  -- line used for closed folds
+      on_colors = function(colors)
+        -- with "night" and dim_inactive, floats end up with the same color
+        -- as the code window, which makes things very confusing. Override floats
+        -- to something different.
+        colors.bg_float = "#000000"
       end,
 
-      on_colors = function(colors)
-        colors.bg_float = "#1f202d"
-      end
+      on_highlights = function(hl, c)
+        hl.Comment = { fg = "#727ca7" } -- bit more contrast
+        hl.Folded = { bg = "#1a1b26" }  -- line used for closed folds
+
+        -- Override telescope backgrounds. The foregrounds are all original.
+        -- This is because we overrode bg_float above, but since telescope is
+        -- full screen, I don't want it looking like a float.
+        hl.TelescopeNormal = {
+          bg = c.bg,
+          fg = c.fg_dark,
+        }
+        hl.TelescopePromptTitle = {
+          bg = c.bg_dark,
+          fg = c.fg,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.border_highlight,
+        }
+        hl.TelescopePromptBorder = {
+          bg = c.bg_dark,
+          fg = c.orange,
+        }
+      end,
     })
 
     vim.cmd([[colorscheme tokyonight]])
