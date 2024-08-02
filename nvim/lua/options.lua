bf2267bfc126934ad19a1274376a1524c6ac000e
Travis Collins <travistx@gmail.com>
2024-08-01 18:23:04 -0600

    word wrap markdown files by default

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -78,3 +78,10 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.wo.scrolloff = 0
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})
