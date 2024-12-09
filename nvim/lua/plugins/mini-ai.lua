b659f0c6e30b8f6f8ec31f6cce0d44deea07fedd
Travis Collins <travistx@gmail.com>
2024-12-08 17:25:16 -0700

    function text object

--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -2,7 +2,28 @@
 -- `vib`/`vab` select brackets
 return {
   "echasnovski/mini.ai",
-  config = function()
-    require("mini.ai").setup()
+  event = "VeryLazy",
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({ -- code block
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }),
+        f = ai.gen_spec.treesitter({ -- function
+          a = "@function.outer",
+          i = "@function.inner"
+        }),
+        c = ai.gen_spec.treesitter({ -- class
+          a = "@class.outer",
+          i = "@class.inner"
+        }),
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("mini.ai").setup(opts)
   end,
 }
