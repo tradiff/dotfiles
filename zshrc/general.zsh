23a6ed85315a10402dfbbf10cb818bdd3af4c335
Travis Collins <travistx@gmail.com>
2025-06-04 20:35:47 -0600

    a few zsh bindings and settings

--- zshrc/general.zsh
+++ zshrc/general.zsh
@@ -5,7 +5,6 @@ setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_FIND_NO_DUPS
 setopt HIST_SAVE_NO_DUPS
-setopt hist_ignore_space
 
 HISTFILE=~/.zsh_history
 HISTSIZE=1000
@@ -16,8 +15,11 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search # Up arrow
+bindkey "^[[A" up-line-or-beginning-search   # Up arrow
 bindkey "^[[B" down-line-or-beginning-search # Down arrow
+
+# don't highlight text after pasting it
+zle_highlight=('paste:none')
 
 # case insensitive path-completion
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
@@ -36,11 +38,17 @@ export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:/usr/sbin"
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$MAVEN_HOME/bin
+export PATH="$PATH:/opt/gradle/gradle-8.9/bin"
+export PATH="$PATH:/home/travis/Applications/idea-IU-243.24978.46/bin"
+export MAVEN_HOME=/opt/maven
 
 alias ls='eza -lha -F=always --git --group-directories-first'
 alias cls='clear && printf "\e[3J"'
 alias k="kubectl"
 alias zy="sudo zypper"
+alias dnf="sudo dnf"
 
 function lc() {
     if [ -d "$1" ]; then
@@ -58,13 +66,15 @@ export EDITOR="nvim"
 export DIFFPROG="nvim -d $1"
 
 bindkey "^[[1~" beginning-of-line # home
-bindkey "^[[4~" end-of-line # end
-bindkey "^[[1;5C" forward-word # ctrl + right
-bindkey "^[[1;5D" backward-word # ctrl + left
-bindkey "^[" kill-whole-line # esc
+bindkey "^[[4~" end-of-line       # end
+bindkey "^[[1;5C" forward-word    # ctrl + right
+bindkey "^[[1;5D" backward-word   # ctrl + left
+bindkey "^[" kill-whole-line      # esc
+bindkey "\C-?" backward-delete-char
+bindkey "\C-h" backward-kill-word 
+bindkey "[3~" delete-char # Delete
 
 # ctrl+z to toggle between open text editors
 bindkey -s '^Z' '^Ufg^M'
 
 source ~/secrets.zsh
-
