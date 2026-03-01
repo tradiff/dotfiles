f1c11c2d55f50b05330c3fdfdc6e93dda3892abc
Travis Collins <travistx@gmail.com>
2026-02-28 20:25:03 -0700

    rails rspecp command for fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -19,6 +19,7 @@ set -gx MAVEN_HOME /opt/maven
 set -gx WORKER_TIMEOUT 3600
 set -gx EDITOR nvim
 set -gx DIFFPROG "nvim -d"
+set -gx NVIM_APPNAME nvim-lazy
 
 ################################################################################
 # Aliases
@@ -44,6 +45,26 @@ bind \e\[1\;5C nextd-or-forward-word   # Ctrl+Right
 bind \cl 'cls; commandline -f repaint'
 
 bind tab complete-and-search
+
+################################################################################
+# Rails
+################################################################################
+function rspecp
+    set start (date +%s)
+    bundle exec turbo_tests -n10 --require ~/.config/ruby/rspec_formatter.rb --format RspecFormatter $argv
+    set test_status $status
+    set end (date +%s)
+    set duration (math $end - $start)
+    set minutes (math $duration / 60)
+    set seconds (math $duration % 60)
+
+    if test $test_status -eq 0
+        notify-send --icon="messagebox_info" --app-name="turbo tests" --transient "tests passed 🎉" "in {$minutes}m {$seconds}s"
+    else
+        notify-send --icon="messagebox_critical" --app-name="turbo tests" --transient "tests failed" "in {$minutes}m {$seconds}s"
+    end
+    paplay ~/.config/dunst/gaming-lock.wav
+end
 
 ################################################################################
 # Atuin
